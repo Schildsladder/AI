7bce0947289c92c58a2dc8af1030109ef0b06c61
Gesit Tali Singgih <gesitsinggih2@gmail.com>
2023-01-25 10:32:45 +0700

    trying vs code

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # AI
-My implementation of historical AI paper froom scratch using cupy and numpy
+Implementation of historical AI paper froom scratch using cupy and numpy
--- diffusion/UNet.py
+++ diffusion/UNet.py
@@ -5,6 +5,17 @@ Created on Tue Jan 24 22:05:00 2023
 @author: gesit
 """
 
+import matplotlib.pyplot as plt
+import numpy as np
+# import pickle as pkl
+from tqdm import tqdm
+from PIL import Image
+
+
+import sys
+sys.path.append(r"D:\SE\Python\AI")
+
+from cupy.data import file_process, Images
 
 
 
